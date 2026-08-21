89a86f2fa722eda44a1ac7f9dc3ec5f35ea953aa
Renzo Rico <renzorico10@gmail.com>
2026-08-21 10:00:08 +0200

    testing again

--- app.py
+++ app.py
@@ -1,3 +1,13 @@
 import streamlit as st
 
 st.text('hello')
+
+
+spell = st.secrets['spell']
+
+st.write(spell)
+
+st.markdown('''
+# HELLO PEOPLE FROM BATCH 2332
+## THIS IS A SUBTITLE
+            ''')
